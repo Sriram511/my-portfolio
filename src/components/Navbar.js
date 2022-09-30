d0ccaaf9d34b5d2f9cb74d03f23fd55ff2c5f9d8
Lakshmi Narasimhan S <bachelorofphotography@gmail.com>
2022-09-30 14:33:46 +0530

    Saved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import * as FaIcons from "react-icons/fa";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const links = [
+    {
+      id: 1,
+      link: "home",
+    },
+    {
+      id: 2,
+      link: "about",
+    },
+    {
+      id: 3,
+      link: "experience",
+    },
+    {
+      id: 4,
+      link: "portfolio",
+    },
+    {
+      id: 5,
+      link: "contact",
+    },
+  ];
+
   return (
-    <div className="flex justify-between items-center w-full h-20 text-white">
+    <div className="flex justify-between items-center w-full h-20 text-white bg-black px-4 fixed">
       <div>
-        <h1>Testing</h1>
+        <h1 className="text-3xl italic font-signature ml-3">SL Narasimhan</h1>
       </div>
+
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => (
+          <li
+            key={id}
+            className="px-4 cursor-pointer capitalize font-medium text-gray-400 hover:scale-105 duration-200"
+          >
+            {link}
+          </li>
+        ))}
+      </ul>
+
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-3 z-10 text-gray-400 md:hidden"
+      >
+        {nav ? <FaIcons.FaTimes size={25} /> : <FaIcons.FaBars size={25} />}
+      </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute w-full h-screen top-0 left-0 bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
