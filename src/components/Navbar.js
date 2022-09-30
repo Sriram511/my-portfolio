a5acd1870306c3a4ce0d2881a8ae96c1b98e578f
Lakshmi Narasimhan S <bachelorofphotography@gmail.com>
2022-09-30 12:59:09 +0530

    Saved

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center w-full h-20 text-white">
+      <div>
+        <h1>Testing</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
