aa89baee893c321377086373d66b7083955b8831
Lakshmi Narasimhan S <bachelorofphotography@gmail.com>
2022-10-07 19:36:06 +0530

    Starting Portfolio Section
    Just starting portfolio section the time duration is 01 : 09 : 54

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ const About = () => {
             About
           </p>
         </div>
-        <p className="text-xl mt-32">
+        <p className="text-xl mt-24">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
