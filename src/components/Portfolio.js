6dcddce8bf58751c391692e38ccd05756160133f
Lakshmi Narasimhan S <bachelorofphotography@gmail.com>
2022-10-08 16:50:14 +0530

    images added
    images added through in the main origin

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,7 @@
 import React from "react";
+import chatApp from "../assets/portfolio/chatApp.png";
+import crudApp from "../assets/portfolio/crud.png";
+import weatherPY from "../assets/portfolio/weatherPY.png";
 
 const Portfolio = () => {
   return <div>Portfolio</div>;
